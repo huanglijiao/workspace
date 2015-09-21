2a4d25915b1a6354cb597cb38f7e16ed0166b57f
FrancesShih <shiyongxin_327@aliyun.com>
2015-09-21 15:52:56 +0800

    gulpfile
    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+*@file gulp
+*/
+// require
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var ejs = require('gulp-ejs');
+var util = require('gulp-util');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var minicss = require('gulp-minify-css');
+vim minihtml = require('gulp-minify-html');
+var rename = require('gulp-rename');
+var sourcemap = require('gulp-sourcemaps');
+
+
+
